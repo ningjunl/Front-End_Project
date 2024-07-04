88899a8edc5259a665073420eb4d97517838ac6e
Nelson <nelsonliningjun@gmail.com>
2024-07-04 02:33:37 -0700

    text-and image

--- React_Native/First-Project/App.js
+++ React_Native/First-Project/App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
+  let x = 1;
+  const handlePress = () => console.log('Text pressed', x++);
+  console.log('App executed');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress} style={styles.text}>
+        Hello React Native
+      </Text>
+      <Image blurRadius={10}
+        fadeDuration={1000}
+      source={{
+
+        uri: "https://picsum.photos/200/300"
+
+      }} style={{width:200, height:300, resizeMode: 'contain',  }}/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'dodgerblue',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  text: {
+    marginTop: 0, // 调整这个值以控制向下移动的距离
+    color: 'white', // 可选：改变文字颜色以便更好地对比背景
+  },
+  
 });
